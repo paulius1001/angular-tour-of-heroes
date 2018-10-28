e3d63f94cfcf7ea3e8a6a5af1a9cd753aa80280e
PmCreator <paulius1001@gmail.com>
2018-10-28 21:51:57 +0200

    The Hero Editor (Step 1)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { Heroes1Component } from './heroes1/heroes1.component';
+
+const routes: Routes = [
+	{
+		path: 'hero_step_1',
+		component: Heroes1Component
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
+import { Heroes1Component } from './heroes1/heroes1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Heroes1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
